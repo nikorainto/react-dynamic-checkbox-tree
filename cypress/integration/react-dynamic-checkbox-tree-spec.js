8d2ab8267f1c70e193c51ccb7475103d608a7546
Jaws <jawsnotfound1@gmail.com>
2019-11-03 23:23:15 +0200

    Updated tests

--- cypress/integration/react-dynamic-checkbox-tree-spec.js
+++ cypress/integration/react-dynamic-checkbox-tree-spec.js
@@ -1,20 +1,81 @@
 /* eslint-disable no-undef */
 // @ts-nocheck
-describe('react-dynamic-checkbox-tree', function() {
-  it('can find all checkboxes, labels and icons', function() {
+describe('react-dynamic-checkbox-tree', () => {
+  it('can find all checkboxes, labels and icons', () => {
     cy.visit('http://localhost:8888/')
 
     cy.contains('react-dynamic-checkbox-tree')
-    cy.get('input').should('have.length', 4)
-    cy.get('label').should('have.length', 4)
-    cy.get('svg').should('have.length', 3)
+    cy.fixture('data').then(nodes => {
+      const nodesWithChildren = nodes
+        .map(node => (node.children ? 1 : 0))
+        .reduce((a, b) => a + b, 0)
+      cy.get('input').should('have.length', nodes.length)
+      cy.get('label').should('have.length', nodes.length)
+      cy.get('svg').should('have.length', nodesWithChildren)
+    })
   })
 
-  it('can check the four checkboxes', function() {
+  it('can check the four root checkboxes', () => {
     cy.fixture('data').then(nodes => {
       nodes.forEach(node => {
         cy.get(`#${node.id}`).check()
       })
+    })
+
+    cy.get('input').should('be.checked')
+  })
+
+  it('can extend the nodes', () => {
+    cy.fixture('data').then(nodes => {
+      const extendNodes = nodes => {
+        nodes.forEach(node => {
+          cy.get(`#${node.id}`)
+            .siblings('span')
+            .click()
+
+          if (node.children) {
+            extendNodes(node.children)
+          }
+        })
+      }
+      extendNodes(nodes)
+    })
+  })
+
+  it('all checkboxes should be visible and checked', () => {
+    cy.fixture('data').then(nodes => {
+      let length = 0
+      const getLengthOfAllNodes = nodes => {
+        nodes.forEach(node => {
+          length++
+          if (node.children) {
+            getLengthOfAllNodes(node.children)
+          }
+        })
+      }
+      getLengthOfAllNodes(nodes)
+      nodes.forEach(node => {
+        cy.get(`#${node.id}`).should('be.checked')
+      })
+      cy.get(`input`).should('have.length', length)
+    })
+  })
+
+  it('checkbox indeterminate works', () => {
+    cy.fixture('data').then(nodes => {
+      cy.get(`#${nodes[0].children[0].id}`).uncheck()
+      cy.get(`#${nodes[0].id}`).should('not.be.checked')
+    })
+  })
+
+  it('can collapse the nodes', () => {
+    cy.fixture('data').then(nodes => {
+      nodes.forEach(node => {
+        cy.get(`#${node.id}`)
+          .siblings('span')
+          .click()
+      })
+      cy.get('input').should('have.length', nodes.length)
     })
   })
 })
